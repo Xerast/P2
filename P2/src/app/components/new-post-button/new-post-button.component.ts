d4e38ac10c93749ee715b2f3492e779012038c1e
Jorge Tinajero <jotinajero07@hotmail.com>
2021-10-22 10:25:47 -0500

    combined button and new post

--- P2/src/app/components/new-post-button/new-post-button.component.ts
+++ P2/src/app/components/new-post-button/new-post-button.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {Post} from 'src/app/model/post';
+import {CreatePostService} from 'src/app/services/create-post.service'
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+
+
+function openForm(){
+  console.log("inside openform")
+  document.getElementById("myForm")!.style.display = "block";
+}
+
+function closeForm(){
+  document.getElementById("myForm")!.style.display = "none";
+}
 
 @Component({
   selector: 'app-new-post-button',
@@ -7,14 +20,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./new-post-button.component.css']
 })
 export class NewPostButtonComponent implements OnInit {
+  postData : Post | any;
 
   constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void{
   }
 
   newPost(){
-    this.router.navigateByUrl(`/post-form`);
+    openForm();
+  }
+
+  onsubmit(newPost : NgForm){
+    console.log(newPost.value);
+    const postData = new Post(newPost.value.id,newPost.value.title, newPost.value.body, newPost.value.res_url, newPost.value.rest_id, newPost.value.recommend, newPost.value.rating);
+  }
+
+  cancel(){
+    closeForm();
   }
 
 }
