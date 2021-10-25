f4b25c78a0c3eb849231b6fc063f015681838821
Shailendra Nain <shailnain.6@gmail.com>
2021-10-25 16:37:45 -0400

    Navbar added

--- foodInstagram/src/main/java/com/revature/foodinstagram/beans/Post.java
+++ foodInstagram/src/main/java/com/revature/foodinstagram/beans/Post.java
@@ -3,7 +3,7 @@ package com.revature.foodinstagram.beans;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
+
 
 import javax.persistence.*;
 import java.io.Serializable;
