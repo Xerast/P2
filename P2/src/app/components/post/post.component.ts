f98afe99964b93ab10dd2276ea1ba75af25c3919
Xerast <scarnati.james@gmail.com>
2021-10-25 16:41:13 -0500

    comments = displayed

--- P2/src/app/components/post/post.component.ts
+++ P2/src/app/components/post/post.component.ts
@@ -4,19 +4,28 @@ import { Post } from 'src/app/model/post';
 import { PostService } from 'src/app/services/post.service';
 import { Comment } from 'src/app/model/comment';
 import { User } from 'src/app/model/user';
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 export class PostComponent implements OnInit {
   
   posts: Post[] = [];
   comments: Comment[] = [];
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 
   food = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/12/unhealthiest-foods-planet.jpg"
   constructor(private postService: PostService, private router: Router) { }
--- foodInstagram/src/main/java/com/revature/foodinstagram/services/PostServices.java
+++ foodInstagram/src/main/java/com/revature/foodinstagram/services/PostServices.java
@@ -44,11 +44,17 @@ public class PostServices {
             for(int j = 0; j < cLength; j++){
                 Comment c = comments.get(j);
                 if( p.getId() == c.getPostId()){
+<<<<<<< Updated upstream
                     p.setComment(comments);
                     List<Comment> com = p.getComment();
                     com.add(c);
                     p.setComment(com);
 
+=======
+                    List<Comment> com = p.getComment();
+                    com.add(c);
+                    p.setComment(com);
+>>>>>>> Stashed changes
                 }
             }
         }
