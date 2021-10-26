7b540a97d75e41d2533b0181ec2cf1a8cb158d7e
Shailendra Nain <shailnain.6@gmail.com>
2021-10-26 15:40:45 -0400

    remove old code

--- P2/src/app/app.module.ts
+++ P2/src/app/app.module.ts
@@ -32,15 +32,10 @@ const routes: Routes = [
   {path: 'post', redirectTo: 'post', pathMatch: 'full'},
   {path: 'register', component: RegistrationFormComponent},
   {path: 'register', redirectTo: 'register', pathMatch: 'full'},
-<<<<<<< HEAD
-  {path: 'comment', component: CommentComponent},
-  {path: 'comment', redirectTo: 'comment', pathMatch: 'full'}
-=======
   
   {path: 'comment', component: CommentComponent},
   {path: 'comment', redirectTo: 'comment', pathMatch: 'full'}
   
->>>>>>> 473349f0cbd285c803b61ddf328398759f2a83dc
 ];
 
 @NgModule({
--- P2/src/app/components/post/post.component.ts
+++ P2/src/app/components/post/post.component.ts
@@ -4,10 +4,7 @@ import { Post } from 'src/app/model/post';
 import { PostService } from 'src/app/services/post.service';
 import { Comment } from 'src/app/model/comment';
 import { User } from 'src/app/model/user';
-<<<<<<< HEAD
-=======
 import { UpdateCommentsService } from 'src/app/services/comments.service';
->>>>>>> 473349f0cbd285c803b61ddf328398759f2a83dc
 
 @Component({
   selector: 'app-post',
@@ -18,10 +15,7 @@ export class PostComponent implements OnInit {
   
   posts: Post[] = [];
   comments: Comment[] = [];
-<<<<<<< HEAD
-=======
   id = {value: 0};
->>>>>>> 473349f0cbd285c803b61ddf328398759f2a83dc
 
   food = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/12/unhealthiest-foods-planet.jpg"
   constructor(private postService: PostService, private commentService: UpdateCommentsService, private router: Router) { }
